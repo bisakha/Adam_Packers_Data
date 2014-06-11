e6dedda26db03ac354355dba689e62dc9a8febbf
Bisakha Ray <rbisakha@gmail.com>
2014-06-11 17:13:34 -0400

    Create baseline_corrected.m

---
+++ baseline_corrected.m
@@ -0,0 +1,17 @@
+myFolder = 'C:\Users\Bisakha Ray\Desktop\MAT_files_for_ANALYSIS\Analysis\_Details';
+if ~isdir(myFolder)
+  errorMessage = sprintf('Error: The following folder does not exist:\n%s', myFolder);
+  uiwait(warndlg(errorMessage));
+  return;
+end
+filePattern = fullfile(myFolder, '*.dat');
+datFiles = dir(filePattern);
+H115_z150_dfofOut_corrected = [];
+for k = 1:length(datFiles)
+  baseFileName = datFiles(k).name;
+  fullFileName = fullfile(myFolder, baseFileName);
+  fprintf(1, 'Now reading %s\n', fullFileName);
+  time_series = csvread(fullFileName,0,3);
+  H115_z150_dfofOut_corrected = [H115_z150_dfofOut_corrected time_series];
+end
+dlmwrite('C:\Users\Bisakha Ray\Desktop\MAT_files_for_ANALYSIS\Analysis\H115_z150_dfofOut_corrected.txt', H115_z150_dfofOut_corrected, ',');
